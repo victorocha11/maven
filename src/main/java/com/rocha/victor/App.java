7bf323c59e747dd4f7aff7c0c58cbb3ebbd678a8
victor.santos <victorocha11@gmail.com>
2016-06-30 16:24:06 -0300

    commit inicial

---
+++ src/main/java/com/rocha/victor/App.java
@@ -0,0 +1,19 @@
+package com.rocha.victor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Exemplo Maven!" );
+        /*double result=1.01;
+        for(int i=0;i<364;i++){
+        	result*=1.01;
+        	//System.out.println("Resultado: "+result);
+        }
+        System.out.println("Resultado: "+result)*/;
+    }
+}
